79496060b3c25d03a916209949153f7b6ee55978
andreazambujasilva <130559509+andreazambujasilva@users.noreply.github.com>
2023-09-22 00:23:54 -0300

    Update 6ngx-manager.sh

--- 6ngx-manager.sh
+++ 6ngx-manager.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-
-# Função para exibir o banner do arquivo banner.sh
-display_banner() {
-  clear
-  if [ -f banner.sh ]; then
-    source banner.sh
-  else
-    echo "Banner não encontrado. Verifique se o arquivo banner.sh está na mesma pasta."
-  fi
-  sleep 3  # Tempo de exibição do banner (3 segundos)
-}
-
-# Exibe o banner informativo
-display_banner
-
 # Função para verificar se o usuário é root (sudo)
 check_root() {
   if [[ $EUID -ne 0 ]]; then
