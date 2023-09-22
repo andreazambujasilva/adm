ae76ee1cc32bfe906ad33968b00278564b2ddcd0
andreazambujasilva <130559509+andreazambujasilva@users.noreply.github.com>
2023-09-22 00:23:06 -0300

    Update banner2.sh

--- banner2.sh
+++ banner2.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
 
-# Banner de créditos
-echo "
-███████╗██╗  ██╗███████╗    ██╗███████╗██╗  ██╗███████╗
-██╔════╝██║  ██║██╔════╝    ██║██╔════╝██║  ██║██╔════╝
-███████╗███████║███████╗    ██║███████╗███████║███████╗
-╚════██║██╔══██║╚════██║    ██║╚════██║██╔══██║╚════██║
-███████║██║  ██║███████║    ██║███████║██║  ██║███████║
-╚══════╝╚═╝  ╚═╝╚══════╝    ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝
-
-Créditos para André Almeida e ao canal 'Fabricando Sua Ideia Tutoriais'.
-Para mais informações, visite https://www.youtube.com/@fabricandosuaideiatutoriais
-Cláudio Vinicius da comunidade Evolution-Api (cobaia master kkk)
-"
